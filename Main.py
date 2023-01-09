18f2630380e71fcd78a46fd91402554af9212dc7
Matheus Favaretto <116848225+MatheuslFavaretto@users.noreply.github.com>
2023-01-09 07:20:11 -0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,11 @@
+import httpx
+
+MoedaBase = input('Digite a moeda de base: ').upper()
+MoedaDesejada = input('Digite a moeda desejada: ').upper()
+
+# Busca valores de moedas na Api pública
+response = httpx.get (
+url=f'https://api.exchangerate.host/latest?base=(MoedaBase)'
+).json()
+MoedaData = response['rates']
+print(f'1 {MoedaBase} vale {MoedaData.get(MoedaDesejada)} {MoedaDesejada}')
